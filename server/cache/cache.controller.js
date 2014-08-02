4973dcf42c764c90f4c491d8270f08c5f1357f37
valiantvu <michelle.thi.vu@gmail.com>
2014-08-02 00:07:41 -0700

    Rename database-helpers.js to cache.controller.js

---
+++ server/cache/cache.controller.js
@@ -0,0 +1,21 @@
+var Cache = require('../cache/cache.js');
+
+// Creates a new cache
+exports.post = function(req, res) {
+    var newCache = {
+        title: req.body.title,
+        author: req.body.author,
+        content: req.body.content
+    }
+    new Cache(newCache).save();
+}
+
+// Returns a list of caches
+exports.getAll = function(req, res) {
+
+}
+
+// Returns a single cache
+exports.getOne = function(req, res) {
+
+}
