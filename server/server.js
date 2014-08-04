3b0cd6d0a11cd3b754d29e2d2f0045df7a29ccab
valiantvu <michelle.thi.vu@gmail.com>
2014-08-04 11:36:57 -0700

    (feat) Add routes for list, save, getOne, and near to test controller methods

--- server/server.js
+++ server/server.js
@@ -1,23 +1,27 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var app = module.exports = express();
+var seed = require('./stache/stache.seed.js');
+var api = require('./stache/stache.controller.js');
 // var server = require('http').createServer(app);
 
 mongoose.connect('mongodb://localhost/specter');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error: '));
-// db.once('open', callback);
-
-// app.configure(function() {
-//     app.use(express.bodyParser());
-//     app.use(express.methodOverride());
-//     app.use(app.router);
-// });
-
-var api = require('./stache/stache.controller.js');
 
 // routes
+// list all staches
 app.get('/stache', api.list);
+
+// save a new stache
+app.get('/stache/save', api.save);
+
+// get a stache by title
+app.get('/stache/:title', api.getOne);
+
+// get staches nearby specified longitude, latitude
+// within the specified distance
+app.get('/stache/near/:lon-:lat-:dist', api.near);
 
 
 app.listen(8000, function () {
