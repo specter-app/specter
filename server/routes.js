6b4af42f8ee997e33c2dbc7fe3250eea9b3f0ec2
jaredscheib <jared.scheib@gmail.com>
2014-08-04 18:59:40 -0700

    Refactor routes and stache controller so only methods are save, getMany, and getOne

--- server/routes.js
+++ server/routes.js
@@ -8,18 +8,18 @@ var sCtrl = require('./stache/stache.controller.js');
 var seed = require('./stache/stache.seed.js');
 
 var router = express.Router()
-    
-router.get('/', function(req, res) {
-  console.log('get staches. request body:', req.body);
-  sCtrl.list(req, res);
-  // // get staches nearby specified longitude, latitude
-  // // within the specified distance
-  // app.get('/stache/near/:lon-:lat-:dist', api.near);
-});
+
+//staches/283497a823947hj
+//staches?=
 
 router.get('/:id', function(req, res) {
   console.log('get stache with id. request body:', req.body);
   sCtrl.getOne(req, res); //title, UID, or something else for individual caches?
+});
+    
+router.get('/', function(req, res) {
+  console.log('get staches near', req.query, '. request body:', req.body);
+  sCtrl.getMany(req, res);
 });
 
 router.post('/', function(req, res) {
