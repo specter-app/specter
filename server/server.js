d15a9ea92041ab6b8882e07aa60229766b2b2ea0
valiantvu <michelle.thi.vu@gmail.com>
2014-08-06 11:49:51 -0700

    (feat) Add mongolab url

--- server/server.js
+++ server/server.js
@@ -7,6 +7,16 @@ var server = app.listen(app.get('port'), function(){
   console.log('Server listening on port', server.address().port);
 });
 
-mongoose.connect('mongodb://localhost/specter');
+var production = 'mongodb://MongoLab-p:0y_6JMYwAj8Mm9Q1cl0D2SHWTxskyt21DSnuzeGnOAk-@ds050077.mongolab.com:50077/MongoLab-p';
+var development = 'mongodb://localhost/specter';
+var mongoUri = production;
+
+/*
+var mongoUri = process.env.MONGOLAB_URI ||
+    process.env.MONGOHQ_URL ||
+    'mongodb://localhost/specter';*/
+
+mongoose.connect(mongoUri);
 var db = mongoose.connection;
+
 db.on('error', console.error.bind(console, 'connection error: '));
