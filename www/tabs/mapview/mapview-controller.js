5c7e31aa56b8a201128b7b8d9f616799dd1a88c8
Bao <baoducng@gmail.com>
2014-08-06 15:37:14 -0700

    completed simple listview in mapView

--- www/tabs/mapview/mapview-controller.js
+++ www/tabs/mapview/mapview-controller.js
@@ -1,13 +1,5 @@
 angular.module('specter.tab.mapview.controller', ['restangular'])
 .controller('mapviewCtrl', function($http, Restangular, $scope) {
-  // $http.get('http://localhost:8000/staches')
-  //   .success(function(data){
-  //     console.log(data);
-  //   })
-  //   .error(function(msg){
-  //     console.log(msg);
-  //   })
-
   var staches = Restangular.all('staches');
   staches.getList().then(function(staches){
     $scope.staches = staches;
@@ -15,16 +7,5 @@ angular.module('specter.tab.mapview.controller', ['restangular'])
   }, function(response){
     console.log('error with status', response)
   })
-//   Restangular.all('staches').getList()
-//     .then(function(stache) {
-//       console.log(stache);
-//       $scope.stache = stache;
-//     }).catch(function(err) {
-//       console.log('fuck')
-//       console.log(err)
-//     });
-  // Restangular.all('staches').getList()
-  //   .then(function(staches) {
-  //
-  //   });
+  
 });
