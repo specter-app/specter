207363789c000fd31954553b1d7ae1511cdd12c9
valiantvu <michelle.thi.vu@gmail.com>
2014-08-06 12:27:07 -0700

    (feat) Configure mongolab URI to use process env and default to local host

--- server/server.js
+++ server/server.js
@@ -7,14 +7,10 @@ var server = app.listen(app.get('port'), function(){
   console.log('Server listening on port', server.address().port);
 });
 
-var production = 'mongodb://MongoLab-p:0y_6JMYwAj8Mm9Q1cl0D2SHWTxskyt21DSnuzeGnOAk-@ds050077.mongolab.com:50077/MongoLab-p';
-var development = 'mongodb://localhost/specter';
-var mongoUri = production;
 
-/*
 var mongoUri = process.env.MONGOLAB_URI ||
     process.env.MONGOHQ_URL ||
-    'mongodb://localhost/specter';*/
+    'mongodb://localhost/specter';
 
 mongoose.connect(mongoUri);
 var db = mongoose.connection;
