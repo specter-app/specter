777f04d6aac38309f9fbb3c477e4826e43c4b7b7
Bao <baoducng@gmail.com>
2014-08-07 15:53:35 -0700

    refactor the mapview controller, added a mapviewService

---
+++ www/services/mapviewService.js
@@ -0,0 +1,17 @@
+(function(){
+  'use strict';
+  console.log(1);
+  var mapviewService = function(Restangular){
+    this.getAll = function(){
+      return Restangular.all('staches').getList().then(function(staches){
+        return staches;
+      }, function(response){
+        return ('error with status', response);
+      });
+    }
+  };
+  angular.module('specter').service('mapviewService', [
+    'Restangular',
+    mapviewService
+  ]);
+})();
--- www/tabs/mapview/mapview-controller.js
+++ www/tabs/mapview/mapview-controller.js
@@ -1,11 +1,4 @@
-angular.module('specter.tab.mapview.controller', ['restangular'])
-.controller('mapviewCtrl', function($http, Restangular, $scope) {
-  var staches = Restangular.all('staches');
-  staches.getList().then(function(staches){
-    $scope.staches = staches;
-    console.log(staches)
-  }, function(response){
-    console.log('error with status', response)
-  })
-  
-});
+angular.module('specter.tab.mapview.controller', [])
+.controller('mapviewCtrl', ['$scope', 'mapviewService', function($scope, mapviewService) {
+  $scope.staches = mapviewService.getAll();
+}]);
