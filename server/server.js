2cc79e7bed2b51a417db7de526be3830766e838c
valiantvu <michelle.thi.vu@gmail.com>
2014-08-06 22:43:19 -0700

    (test) Update server.js to connect to database based on node environment

--- server/server.js
+++ server/server.js
@@ -1,15 +1,17 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 var app = require('./server-config.js');
 var mongoose = require('mongoose');
-
+var config = require('./config');
 app.set('port', process.env.PORT || 8000);
 
 var server = app.listen(app.get('port'), function(){
   console.log('Server listening on port', server.address().port);
 });
 
-
-var mongoUri = process.env.CUSTOMCONNSTR_MONGOLAB_URI || 'mongodb://localhost/specter';
-
+// var mongoUri = process.env.CUSTOMCONNSTR_MONGOLAB_URI || 'mongodb://localhost/specter';
+// mongoose.connect(mongoUri);
+var mongoUri = config[process.env.NODE_ENV].mongodb;
 mongoose.connect(mongoUri);
 var db = mongoose.connection;
 
