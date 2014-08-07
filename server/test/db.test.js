1a0c910848740deef60df1c01146272fcb56d2c5
valiantvu <michelle.thi.vu@gmail.com>
2014-08-06 22:46:22 -0700

    (test) Database test file connects to test db and drops db at end

--- server/test/db.test.js
+++ server/test/db.test.js
@@ -1,3 +1,28 @@
+var should = require('should');
 var mongoose = require('mongoose');
+var config = require('../config');
 var stachController = require('../stache/stache.controller.js');
+
+process.env.NODE_ENV = 'test';
+var mongoUri = config[process.env.NODE_ENV].mongodb;
+var db = mongoose.connect(mongoUri);
+
+// trap any errors from mongoose connection
+mongoose.connection.on('error', function(err){
+    console.log(err);
+});
+
+
+
+
+// After tests are done, 
+// drop the database and close the connection
+after(function(done) {
+    db.connection.db.dropDatabase(function() {
+        db.connection.close(function(){
+            done();
+        });
+    });
+});
+
 
