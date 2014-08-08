b74c859b4bfaccf104f563af46446f1d12542ec1
valiantvu <michelle.thi.vu@gmail.com>
2014-08-07 19:54:11 -0700

    (refactor) Configure mongo uri based on NODE_ENV

--- server/server-config.js
+++ server/server-config.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var app = express();
+var mongoose = require('mongoose');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -9,7 +10,17 @@ app.use(express.static('../' + __dirname + '/www'));
 
 app.use(cors());
 
+// Configure based on environment
+console.log('environment: ', app.get('env'));
+var config = require('./config/' + app.get('env') + '.js');
 
+// Connect to mongodb instance
+var mongoUri = config.mongodb;
+mongoose.connect(mongoUri);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '));
+
+// Routes
 app.use('/staches', require('./routes.js'));
 
 // Create 404 error response for any non-existent REST endpoints
