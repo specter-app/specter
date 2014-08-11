8036d04ae127c2064c4f6c0271e8be360fffaa42
valiantvu <michelle.thi.vu@gmail.com>
2014-08-11 11:12:27 -0700

    (feat) Add map object to scope in marcopolo controller

--- www/tabs/marcopolo/marcopolo-controller.js
+++ www/tabs/marcopolo/marcopolo-controller.js
@@ -1,6 +1,13 @@
 angular.module('specter.tab.marcopolo.controller', [])
-  .controller('marcopoloCtrl', ['$scope', 'geoService', 
+  .controller('marcopoloCtrl', ['$scope', 'geoService',
     function($scope, geoService) {
+      $scope.map = {
+        center: {
+            latitude: 45,
+            longitude: -73
+        },
+        zoom: 8
+      };
       geoService.getLocation().then(function(location) {
         $scope.location = location;
       }, function(err) {
