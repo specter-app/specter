97bfb17001c05f58596b50add171a4154ad066e1
Bao <baoducng@gmail.com>
2014-08-11 12:27:24 -0700

    feat/customDirective created folders for customDirective, got gps location to update on phone

--- platforms/ios/www/tabs/marcopolo/marcopolo-controller.js
+++ platforms/ios/www/tabs/marcopolo/marcopolo-controller.js
@@ -1,9 +1,30 @@
 angular.module('specter.tab.marcopolo.controller', [])
-  .controller('marcopoloCtrl', ['$scope', 'geoService', 
-    function($scope, geoService) {
-      geoService.getLocation().then(function(location) {
-        $scope.location = location;
+  .controller('marcopoloCtrl', ['$scope', 'geoService', '$cordovaGeolocation',
+    function($scope, geoService, $cordovaGeolocation) {
+      $scope.location = {long: "", lat: ""}
+      $scope.map = {
+        center: {
+            latitude: 37.7837,
+            longitude: -122.4089
+        },
+        zoom: 15
+      };
+      geoService.getLocation().then(function(position) {
+        $scope.location.long = position.coords.latitude;
+        $scope.location.lat = position.coords.longitude;
       }, function(err) {
         $scope.location = err;
+
+      });
+      var watch = $cordovaGeolocation.watchPosition({
+        frequency: 1000
+      });
+      watch.promise.then(function() {
+          // Not currently used
+        }, function(err) {
+          $scope.location = err
+        }, function(position) {
+      $scope.location.long = position.coords.latitude;
+      $scope.location.lat = position.coords.longitude;
       });
   }]);
---
+++ www/directive/marcopoloDirective.js
@@ -0,0 +1,4 @@
+angular.module('marcopoloDirective',[])
+  .directive('marcopoloDirective', function(){
+    
+  });
