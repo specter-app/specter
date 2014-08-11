db9931a637ddbee666bfeaea8f88bd4877e88a95
jaredscheib <jared.scheib@gmail.com>
2014-08-11 13:23:45 -0700

    (cleanup) Remove server variable

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ var app = require('./server-config.js');
 
 app.set('port', process.env.PORT || 8000);
 
-var server = app.listen(app.get('port'), function(){
-  console.log('Server listening on port', server.address().port);
+app.listen(app.get('port'), function(){
+  console.log('Server listening on port', app.get('port'));
 });
 
 module.exports = app;
