a201b7d372f743c79ecdec3b75d13d733f24b502
Bao <baoducng@gmail.com>
2014-08-12 15:40:57 -0700

    added location calculation

--- www/services/geoService.js
+++ www/services/geoService.js
@@ -9,9 +9,9 @@
           });
     };
     this.calculateDistance = function(lon1, lat1, lon2, lat2){
-      var from = new google.maps.LatLng(lon1, lat1);
-      var to = new google.maps.LatLng(lon2, lat2);
-      dist = google.maps.geometry.spherical.computeDistanceBetween(from, to) * 0.000621371192;
+      var from = new google.maps.LatLng(lat1, lon1);
+      var to = new google.maps.LatLng(lat2, lon2);
+      var dist = google.maps.geometry.spherical.computeDistanceBetween(from, to) * 0.000621371192;
         return dist = dist - dist % 0.001;
     };
   };
