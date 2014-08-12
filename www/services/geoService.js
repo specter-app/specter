cd32a59e2d55da57656ce2b49cdc06f8ec3c646a
Bao <baoducng@gmail.com>
2014-08-12 14:51:49 -0700

    added geoservice

--- www/services/geoService.js
+++ www/services/geoService.js
@@ -8,6 +8,12 @@
             return err;
           });
     };
+    this.calculateDistance = function(lon1, lat1, lon2, lat2){
+      var from = new google.maps.LatLng(lon1, lat1);
+      var to = new google.maps.LatLng(lon2, lat2);
+      dist = google.maps.geometry.spherical.computeDistanceBetween(from, to) * 0.000621371192;
+        return dist = dist - dist % 0.001;
+    };
   };
   angular.module('specter').service('geoService', [
     '$cordovaGeolocation',
