f390b6b3ff24493a4e46f607f52b25057d25097c
Bao <baoducng@gmail.com>
2014-08-13 11:48:15 -0700

    feat/ got the rerouting to work, added staches service to discover tabs, update calculate geodistance function with the new location of the stache

--- platforms/ios/www/tabs/mapview/mapview-controller.js
+++ platforms/ios/www/tabs/mapview/mapview-controller.js
@@ -3,5 +3,14 @@ angular.module('specter.tab.mapview.controller', [])
   var self = this;
   stacheService.getAll().then(function(staches) {
     self.staches = staches;
+  self.redirect = function(id){
+    stacheService.getOne(id).then(function(stache){
+      // console.log(stache)
+      // href="#/tab/marcopolo/:{{stache._id}}"
+    }, function(err){
+      return err
+    })
+  }
+
   });
 }]);
--- www/tabs/marcopolo/marcopolo-controller.js
+++ www/tabs/marcopolo/marcopolo-controller.js
@@ -1,8 +1,20 @@
 angular.module('specter.tab.marcopolo.controller', [])
-  .controller('marcopoloCtrl', ['heatmapService', 'geoService', '$cordovaGeolocation', '$scope', 'location',
-    function(heatmapService, geoService, $cordovaGeolocation, $scope, location) {
+  .controller('marcopoloCtrl', [
+      'geoService',
+      '$cordovaGeolocation',
+      '$scope',
+      'location',
+      '$stateParams',
+      'stacheService',
+    function(geoService, $cordovaGeolocation, $scope, location, $stateParams, stacheService) {
       var self = this;
       self.location = {long: "", lat: ""};
+      self.id = $stateParams.id.slice(1);
+      stacheService.getOne(self.id).then(function(stache){
+          self.currentStache = stache;
+        }, function(err){
+          return err
+        })
       self.location.long = location.coords.longitude;
       self.location.lat = location.coords.latitude;
 
@@ -16,11 +28,11 @@ angular.module('specter.tab.marcopolo.controller', [])
 
       HeatLayer = function (heatLayer) {
           var map, pointarray, heatmap;
-          
+
           // TODO: Store heatmap data points in local storage when app closes
           // localStorage.setItem(taxiData, JSON.stringify(taxiData));
           // var pointArray = new google.maps.MVCArray(JSON.parse(localStorage.getItem(taxiData)));
-          
+
           // Test adding a new point
           var hackreactor = heatmapService.addPoint('stache2', 37.783792, -122.408997, 20);
 
@@ -72,7 +84,6 @@ angular.module('specter.tab.marcopolo.controller', [])
         }, function(position) {
       self.location.long = position.coords.longitude;
       self.location.lat = position.coords.latitude;
-      self.distance = geoService.calculateDistance(-122.27013, 37.87487, self.location.long, self.location.lat)
+      self.distance = geoService.calculateDistance(self.currentStache.loc[0], self.currentStache.loc[1], self.location.long, self.location.lat)
       });
-
   }]);
