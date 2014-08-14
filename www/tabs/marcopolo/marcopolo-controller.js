457f8ce436beb0fa97b8840f55896c3147d0229e
valiantvu <michelle.thi.vu@gmail.com>
2014-08-14 00:09:45 -0700

    (feat) Re-render heatmap with new data whenever user has moved to new location

--- www/tabs/marcopolo/marcopolo-controller.js
+++ www/tabs/marcopolo/marcopolo-controller.js
@@ -29,17 +29,15 @@ angular.module('specter.tab.marcopolo.controller', [])
 
       HeatLayer = function (heatLayer) {
           var map, pointarray, heatmap;
-
           // TODO: Store heatmap data points in local storage when app closes
           // localStorage.setItem(taxiData, JSON.stringify(taxiData));
           // var pointArray = new google.maps.MVCArray(JSON.parse(localStorage.getItem(taxiData)));
 
-          // Test adding a new point
-          var hackreactor = heatmapService.addPoint('stache2', 37.783792, -122.408997, 20);
-
           // Get all data points for heatmap
           $scope.pointArray = heatmapService.getPoints('stache1');
           heatLayer.setData($scope.pointArray);
+          // Test adding a new point
+          // var hackreactor = heatmapService.addPoint('stache1', 37.783792, -122.408997, 20);
 
       //function changeGradient() {
       //    var gradient = [
@@ -68,11 +66,11 @@ angular.module('specter.tab.marcopolo.controller', [])
       //function changeOpacity() {
       //    heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
       //}
+        return heatLayer;
       };
 
       $scope.heatLayerCallback = function (layer) {
-        //set the heat layers backend data
-        var heatLayer = new HeatLayer(layer);
+        $scope.heatLayer = new HeatLayer(layer);
       };
 
       var watch = $cordovaGeolocation.watchPosition({
@@ -83,8 +81,30 @@ angular.module('specter.tab.marcopolo.controller', [])
         }, function(err) {
           self.location = err;
         }, function(position) {
-      self.location.long = position.coords.longitude;
-      self.location.lat = position.coords.latitude;
-      self.distance = geoService.calculateDistance(self.currentStache.loc[0], self.currentStache.loc[1], self.location.long, self.location.lat);
+          self.location.long = position.coords.longitude;
+          self.location.lat = position.coords.latitude;
+          self.prevDistance = self.distance;
+          self.distance = geoService.calculateDistance(self.currentStache.loc[0], self.currentStache.loc[1], self.location.long, self.location.lat);
+          // console.log("prevDistance: ", self.prevDistance);
+          // console.log("distance: ", self.distance);
+
+          // Check if user has move by at least 50 meters (0.3 miles) 
+          // from previous location
+          // TODO: Check against existing heat map data
+          if (self.prevDistance && self.prevDistance - self.distance >= 0) {
+            console.log("User has traveled, adding new location to heatmap.");
+            
+            // Temporary - Test adding a new point
+            var hackreactor = heatmapService.addPoint('stache1', 37.783792, -122.408997, 20);
+            $scope.pointArray = heatmapService.getPoints('stache1');
+            
+            // Add current location to heatmap
+            // heatmapService.addPoint(self.id, self.location.lat, self.location.long, self.distance);
+            // $scope.pointArray = heatmapService.getPoints(self.id);
+          }
       });
+      $scope.$watch('pointArray', function (pointArray) {
+        console.log('watch on pointArray triggered');
+        $scope.heatLayer.setData(pointArray);
+      }, true);
   }]);
