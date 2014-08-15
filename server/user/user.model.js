3a36b3e7464717741ae389fb185ec40ea5c6bb05
jaredscheib <jared.scheib@gmail.com>
2014-08-14 18:57:50 -0700

    (feat) Complete initial User database schema in Mongoose

--- server/user/user.model.js
+++ server/user/user.model.js
@@ -5,9 +5,10 @@ var Schema = mongoose.Schema;
 // User collection
 var UserSchema = new Schema({
   user_id: Schema.ObjectId,
-  username: {type: String, required: true},
-  hashedPassword: {type: String, required: true},
+  username: {type: String, required: true, validate: /\S+/},
+  hashedPassword: {type: String, required: true, validate: /\S+/},
   salt: {type: String, required: true},
+  token: [{type: String, required: true}, {type: Date, required: true}],
   stachesCreated: [String],
   stachesDiscovered: [[String, Date, [{lat: Number, lon: Number, dist: Number}]]],
   currentHeatMap: [[Number, Number, Number]],
