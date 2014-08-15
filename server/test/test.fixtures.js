d832370d61f864f733e383f0f9bd97e07e356d2a
jaredscheib <jared.scheib@gmail.com>
2014-08-14 19:28:30 -0700

    (feat) Add fixture for new user signing up

--- server/test/test.fixtures.js
+++ server/test/test.fixtures.js
@@ -1,33 +1,53 @@
 module.exports.testStache1 = {
-    title: 'Advice From Yoda',
-    author: 'Yoda',
-    lon: 40,
-    lat: 6,
-    loc: [40, 6],
-    content: 'Train yourself to let go of everything you fear to lose.',
-    tags: ['Wise', 'Advice'],
-    locked: true,
-    clue: 'Training Plan',
-    password: 'L3tG0'
+  title: 'Advice From Yoda',
+  author: 'Yoda',
+  lon: 40,
+  lat: 6,
+  loc: [40, 6],
+  content: 'Train yourself to let go of everything you fear to lose.',
+  tags: ['Wise', 'Advice'],
+  locked: true,
+  clue: 'Training Plan',
+  password: 'L3tG0'
 };
 
 module.exports.testStache2 = {
-    title: 'Wise Words from Darth Vader',
-    author: 'Darth Vader',
-    lon: 40,
-    lat: 5,
-    loc: [40, 5],
-    content: 'The ability to destroy a planet is insignificant next to the power of the force.',
-    tags: ['Wise', 'Evil']
+  title: 'Wise Words from Darth Vader',
+  author: 'Darth Vader',
+  lon: 40,
+  lat: 5,
+  loc: [40, 5],
+  content: 'The ability to destroy a planet is insignificant next to the power of the force.',
+  tags: ['Wise', 'Evil']
 };
 
 module.exports.testStache3 = {
-    title: 'Veggie Burger',
-    author: 'Hamburglar',
-    lon: 41,
-    lat: 4,
-    loc: [41, 4],
-    content: 'Cow-ered!',
-    tags: ['Food', 'McDonalds'],
-    locked: false
+  title: 'Veggie Burger',
+  author: 'Hamburglar',
+  lon: 41,
+  lat: 4,
+  loc: [41, 4],
+  content: 'Cow-ered!',
+  tags: ['Food', 'McDonalds'],
+  locked: false
 };
+
+// New user without user_id
+module.exports.testUserNew = {
+  username: 'scavenster',
+  password: 'scavengers4evr',
+  firstName: 'Bob',
+  lastName: 'Owen'
+  // profilePhoto: 
+};
+
+// // Existing user with user_id
+// module.exports.testUserExisting = {
+//   user_id: 54ed6f986d6a1658b68f6d17
+//   username: 'stachester',
+  
+//   password: 'staches4lyfe',
+//   firstName: 'Indiana',
+//   lastName: 'Jones',
+
+// };
