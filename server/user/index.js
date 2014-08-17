e55d401fe38a821b89675f04521a2c6efd166b7f
jaredscheib <jared.scheib@gmail.com>
2014-08-16 17:57:27 -0700

    (feat) Add initial server routes for /users/signup and /login

--- server/user/index.js
+++ server/user/index.js
@@ -3,6 +3,16 @@ var userController = require('./user.controller.js');
 
 var users = express.Router();
 
+users.post('/signup', function(req, res){
+  //query database for existing user from req.body
+    //reject / sign in on existing
+    //save on non-existing
+  //send token
+  res.status(201).send();
+});
 
+users.post('/login', function(req, res){
+  res.status(201).send();
+});
 
 module.exports = users;
