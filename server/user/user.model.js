055f465d1a7adc556a4e0d705d367566f307f3ae
jaredscheib <jared.scheib@gmail.com>
2014-08-18 16:36:05 -0700

    (refactor) Update user model to work with Facebook IDs

--- server/user/user.model.js
+++ server/user/user.model.js
@@ -4,17 +4,14 @@ var Schema = mongoose.Schema;
 
 // User collection
 var UserSchema = new Schema({
-  user_id: Schema.ObjectId,
+  user_id: String,
   username: {type: String, required: true, validate: /\S+/},
-  hashedPassword: {type: String, required: true, validate: /\S+/},
-  salt: {type: String, required: true},
-  token: [{type: String, required: true}, {type: Date, required: true}],
-  stachesCreated: [String],
-  stachesDiscovered: [[String, Date, [{lat: Number, lon: Number, dist: Number}]]],
-  currentHeatMap: [[Number, Number, Number]],
-  firstName: String,
-  lastName: String,
-  profilePhoto: String
+  staches_created: [String],
+  staches_discovered: [[String, Date]], // [[stache, date]]
+  current_heatmap: [[Number, Number, String, Date]], // [[lon, lat, color hex, date]]
+  first_name: String,
+  last_name: String,
+  profile_photo: String
 });
 
 var User = mongoose.model('User', UserSchema);
