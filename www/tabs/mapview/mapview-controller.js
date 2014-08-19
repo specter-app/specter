2e12ebf4e7f907e14cbf0393f02ef1100996ba57
valiantvu <michelle.thi.vu@gmail.com>
2014-08-19 12:27:23 -0700

    (cleanup) Store location in object

--- www/tabs/mapview/mapview-controller.js
+++ www/tabs/mapview/mapview-controller.js
@@ -5,7 +5,13 @@ angular.module('specter.tab.mapview.controller', [])
   'location',
   function(stacheService, geoService, location) {
     var self = this;
-    stacheService.getAll({lat: location.coords.latitude, lon: location.coords.longitude, dist: 1000000}).then(function(staches) {
+    var params = {
+      lat: location.coords.latitude,
+      lon: location.coords.longitude,
+      dist: 1000000
+    };
+
+    stacheService.getAll(params).then(function(staches) {
       self.staches = staches;
       console.log(staches);
     });
