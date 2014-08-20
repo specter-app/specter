694ab9f889cb405835ade25d428119f796b0687f
Bao <baoducng@gmail.com>
2014-08-20 10:50:15 -0700

    Revert "added restangular calls to get user staches"
    This reverts commit adbfb861b16015a3e8027bd8a4a9b8b1304506fc.

--- www/services/UserService.js
+++ www/services/UserService.js
@@ -1,19 +1,12 @@
 (function(){
   'use strict';
-  angular.module('specter').factory('UserService', function($rootScope, Restangular){
+  angular.module('specter').factory('UserService', function($rootScope){
     var sdo = {
       uid: 0,
 		  isLogged: false,
 		  username: '',
       pic: ''
-    };
-    sdo.login = function(facebookid){
-      return Restangular.one('users/login', facebookid).get().then(function(staches){
-        return staches;
-      }, function(response){
-        return response;
-      });
-    };
+	  };
     // Upon successful login, set the user object
     $rootScope.$on("$firebaseSimpleLogin:login", function(event, user) {
       sdo.uid = user.uid;
