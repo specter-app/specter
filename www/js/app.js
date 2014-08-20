6390a05e079c61eb11227f8164cae617024ce580
Bao <baoducng@gmail.com>
2014-08-20 15:56:45 -0700

    created a modal to login, refactor code from profile controller into app controller

--- www/js/app.js
+++ www/js/app.js
@@ -4,8 +4,14 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 angular.module('specter', ['ionic', 'specter.tab', 'restangular', 'ngCordova', 'google-maps', 'marcopoloDirective', 'firebase'])
-
-.run(function($ionicPlatform, UserService, $rootScope, $state) {
+.config(function(RestangularProvider) {
+  RestangularProvider.setBaseUrl('http://specter.azurewebsites.net/');
+  RestangularProvider.setDefaultHeaders({
+    "Content-Type": "application/json",
+    "X-Requested-With": "XMLHttpRequest"
+  });
+})
+.run(function($ionicPlatform, UserService, $rootScope) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
@@ -16,20 +22,12 @@ angular.module('specter', ['ionic', 'specter.tab', 'restangular', 'ngCordova', '
   });
   $rootScope.$on('$stateChangeStart', function (event, next) {
     var logInRequired = next.data.logInRequired;
+    // $rootScope.next = next;
     var loggedIn = UserService.isLogged;
     if (!loggedIn && logInRequired) {
        event.preventDefault();
-      $state.go('tab.profile');
+       $rootScope.$emit('$showPopup');
     }
-  });
-
-
-})
-.config(function(RestangularProvider) {
-  RestangularProvider.setBaseUrl('http://specter.azurewebsites.net/');
-  RestangularProvider.setDefaultHeaders({
-    "Content-Type": "application/json",
-    "X-Requested-With": "XMLHttpRequest"
   });
 })
 .filter('distance', function() {
@@ -43,4 +41,4 @@ angular.module('specter', ['ionic', 'specter.tab', 'restangular', 'ngCordova', '
       return distance.toFixed(0) + " meters";
     }
   };
-})
+});
--- www/tabs/profile/profile-controller.js
+++ www/tabs/profile/profile-controller.js
@@ -1,15 +1,3 @@
 angular.module('specter.tab.profile.controller', [])
 .controller("profileCtrl", function($scope, $rootScope, $firebase, $firebaseSimpleLogin, UserService) {
-  // Get a reference to the Firebase
-  var firebaseRef = new Firebase("https://specter-app.firebaseio.com/");
-  // Create a Firebase Simple Login object
-  $scope.auth = $firebaseSimpleLogin(firebaseRef);
-  // Initially set no user to be logged in
-  $scope.user = UserService;
-  $scope.login = function(provider) {
-    $scope.auth.$login(provider);
-  };
-  $scope.logout = function() {
-    $scope.auth.$logout();
-  };
 });
