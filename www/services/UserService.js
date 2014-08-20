adbfb861b16015a3e8027bd8a4a9b8b1304506fc
Bao <baoducng@gmail.com>
2014-08-20 10:38:30 -0700

    added restangular calls to get user staches

--- www/services/UserService.js
+++ www/services/UserService.js
@@ -1,12 +1,19 @@
 (function(){
   'use strict';
-  angular.module('specter').factory('UserService', function($rootScope){
+  angular.module('specter').factory('UserService', function($rootScope, Restangular){
     var sdo = {
       uid: 0,
 		  isLogged: false,
 		  username: '',
       pic: ''
-	  };
+    };
+    sdo.login = function(facebookid){
+      return Restangular.one('users/login', facebookid).get().then(function(staches){
+        return staches;
+      }, function(response){
+        return response;
+      });
+    };
     // Upon successful login, set the user object
     $rootScope.$on("$firebaseSimpleLogin:login", function(event, user) {
       sdo.uid = user.uid;
