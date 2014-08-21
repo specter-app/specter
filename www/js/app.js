a5c884e0ab34a8cd1b701218cb768c587e72800b
Bao <baoducng@gmail.com>
2014-08-21 16:04:07 -0700

    fix error with modal popup

--- www/js/app.js
+++ www/js/app.js
@@ -1,8 +1,4 @@
-// Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
 angular.module('specter', ['ionic', 'specter.tab', 'restangular', 'ngCordova', 'google-maps', 'marcopoloDirective', 'firebase'])
 .config(function(RestangularProvider) {
   RestangularProvider.setBaseUrl('http://specter.azurewebsites.net/');
@@ -21,26 +17,23 @@ angular.module('specter', ['ionic', 'specter.tab', 'restangular', 'ngCordova', '
     }
   });
   $rootScope.$on('$stateChangeStart', function (event, next) {
-    var logInRequired = next.data.logInRequiredg
-    // $rootScope.next = next;
+    var logInRequired = next.data.logInRequired;
     var loggedIn = UserService.isLogged;
     if (!loggedIn && logInRequired) {
        event.preventDefault();
        $rootScope.$emit('$showPopup');
     }
   });
-  //in order to call get all, i need to call geoService,
   geoService.getLocation().then(function(location){
     var params = {
       lat: location.coords.latitude,
       lon: location.coords.longitude,
       dist: 1000000000000
     };
-  stacheService.getAll(params).then(function(staches) {
-    stacheService.selectedStache = staches[0]._id;
+    stacheService.getAll(params).then(function(staches) {
+      stacheService.selectedStache = staches[0]._id;
+    });
   });
-  });
-  //after that i can pass in the params and then call stacheService
 
 })
 .filter('distance', function() {
