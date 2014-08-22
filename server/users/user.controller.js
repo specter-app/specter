93ce7cf5b95ffe8753114d875d0dbbc4f7bfad26
jaredscheib <jared.scheib@gmail.com>
2014-08-21 19:32:30 -0700

    (refactor) Move variable declaration inside for loop to save memory allocation in some cases

--- server/users/user.controller.js
+++ server/users/user.controller.js
@@ -1,18 +1,18 @@
 var User = require('./user.model.js');
 
 exports.login = function(req, res){
-  var user_data = {
-    fbid: req.body.fbid,
-    first_name: req.body.first_name,
-    last_name: req.body.last_name
-    //profilePhoto: String
-  };
-
-  var user = new User(user_data);
-
-  User.findOne({fbid: user.fbid}, function(err, foundUser){
+  User.findOne({fbid: req.body.fbid}, function(err, foundUser){
     if(err) throw err;
     if(!foundUser){
+      var user_data = {
+        fbid: req.body.fbid,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name
+        //profilePhoto: String
+      };
+
+      var user = new User(user_data);
+
       user.save(function(err, savedUser){
         if(err) throw err;
         console.log('savedUser', savedUser.fbid);
