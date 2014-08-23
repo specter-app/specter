ad20a33b67763683fd0da65db9202cb53c5cd6c6
Mitch R <olordhux+github@gmail.com>
2014-08-22 20:43:42 -0700

    when you make a stache, you are congratulated, and ushered to the profile screen

--- www/tabs/create/create-controller.js
+++ www/tabs/create/create-controller.js
@@ -1,6 +1,6 @@
 (function(){
     var createCtrl = function($ionicActionSheet, $ionicPopup, Restangular, cameraService,
-      stacheService, $cordovaCapture, $cordovaGeolocation, geoService, $ionicModal, $scope, ngProgress) {
+      stacheService, $cordovaCapture, $cordovaGeolocation, geoService, $ionicModal, $scope, ngProgress, $state) {
       var progressBar = ngProgress;
       var self = this;
       this.data = {
@@ -148,21 +148,38 @@
         scope: $scope,
         animation: 'slide-in-up'
       }).then(function(modal) {
-        $scope.modal = modal;
+        $scope.privateModal = modal;
+      });
+
+      $ionicModal.fromTemplateUrl('created-modal.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+        }).then(function(modal) {
+          $scope.createdModal = modal;
       });
 
       $scope.$on('$destroy', function() {
-        $scope.modal.remove();
+        $scope.privateModal.remove();
       });
+       
+      this.openCreatedModal = function() {
+        $scope.createdModal.show();
+      };
 
       this.openPrivateModal = function() {
         if(self.checked) {
-          $scope.modal.show();
+          $scope.privateModal.show();
         }
       };
 
       this.closeModal = function() {
-        $scope.modal.hide();
+        $scope.privateModal.hide();
+        $scope.createdModal.hide();
+      };
+
+      this.goToProfile = function() {
+        self.closeModal();
+        $state.go('tab.profile');
       };
 
       //call get location on click of the create button
@@ -175,7 +192,7 @@
 
     };
   createCtrl.$inject = [ '$ionicActionSheet', '$ionicPopup', 'Restangular', 'cameraService',
-    'stacheService', '$cordovaCapture', '$cordovaGeolocation', 'geoService', "$ionicModal",'$scope'];
+    'stacheService', '$cordovaCapture', '$cordovaGeolocation', 'geoService', "$ionicModal",'$scope', '$state'];
   angular.module('specter.tab.create.controller', ['restangular'])
     .controller('createCtrl', createCtrl);
 })();
