45808659e8bbf859730a9bcc3b3c03806e9caa2a
Bao <baoducng@gmail.com>
2014-08-29 14:45:43 -0700

    minor changes

--- www/tabs/profile/profile.js
+++ www/tabs/profile/profile.js
@@ -16,12 +16,9 @@ angular.module('specter.tab.profile', ['specter.tab.profile.controller'])
     .state('tab.profile.discovered', {
       url: "/discovered",
       templateUrl: "tabs/profile/profile-discovered.html"
-      // controller: 'profileCtrl'
     })
     .state('tab.profile.created', {
       url: "/created",
-      // templateUrl: "tabs/profile/profile-created.html",
-      // controller: 'profileCtrl'
       templateUrl: "tabs/profile/profile-created.html"
 
     })
